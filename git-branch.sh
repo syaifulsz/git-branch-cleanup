9bfbbf57ee4b464f140159e539f76f0966f9742d
Syaiful Shah Zinan <i.works@live.com>
2017-04-03 14:51:02 +0800

    Fix bug that causes the bash code for git branches keep on appending the data

--- git-branch.sh
+++ git-branch.sh
@@ -1,2 +1,2 @@
 # Credit http://stackoverflow.com/a/2514279
-for branch in `git branch -r | grep -v HEAD`; do echo -e `git show --format="%ai,%an," $branch | head -n 1` \\t$branch; done | sort -r >> git-branch.csv
+for branch in `git branch -r | grep -v HEAD`; do echo -e `git show --format="%ai,%an," $branch | head -n 1` \\t$branch; done | sort -r > git-branch.csv
